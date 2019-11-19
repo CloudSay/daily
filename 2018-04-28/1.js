8fe8dca7536a6f365624bbd3cc42df80fd3d9cbc
yunfei.huang <yunfei.huang@aliyun.com>
2019-11-19 11:57:32 +0800

    update

--- 2018-04-28/1.js
+++ 2018-04-28/1.js
@@ -1,3 +1,3 @@
-var a:number = 98;
-var b:string  = '3';
+var a: number = 98;
+var b: string = "3";
 console.log(a + b);
--- 2018-04-28/1.ts
+++ 2018-04-28/1.ts
@@ -1,4 +1,4 @@
-let a:number = 98;
-let b:string  = '3';
- b = 0
+let a: number = 98;
+let b: string = "3";
+b = 0;
 console.log(a + b);
